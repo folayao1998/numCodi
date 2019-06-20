45993d0314325d9baeb1b08f7e85deba2426ee64
folayao1998 <35810376+folayao1998@users.noreply.github.com>
2019-06-20 09:08:57 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class principal{
+    public int icodigo;
+
+    public void leerCodigo(int icodigo) {
+        System.out.printl("Ingrese Codigo: ");
+        int icodigo = console.nextInt();
+
+    }
+
+    public void mostrarPrecio() {
+        double precio;
+        return precio;
+    }
+}
